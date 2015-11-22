da94d6f97cc178391db458c99a7782892ee576c7
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2015-11-22 15:59:49 -0800

    generate mine field

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,10 @@
 
 module Main where
 
+import Data.Array (Array, (!), inRange, range, accumArray, listArray)
 import Graphics.Blank (DeviceContext, blankCanvas, send)
+
+import System.Random.Shuffle (shuffleM)
 
 import Colors
 import Draw
@@ -11,8 +14,15 @@ import Draw
 data Game =
   Game { rows    :: Int
        , columns :: Int
+       , field   :: MineField
        }
   deriving Show
+
+type MineField = Array (Int, Int) Item
+
+data Item = Mine
+          | Empty Int
+          deriving Show
 
 bgColor :: Color
 bgColor = dimgrey
@@ -70,7 +80,38 @@ drawGame game@(Game {..}) = do
 main :: IO ()
 main = do
   putStrLn "Go to http://127.0.0.1:3000/"
-  blankCanvas 3000 loop
 
-loop :: DeviceContext -> IO ()
-loop context = send context $ runDraw context $ drawGame (Game 16 30)
+  let cols  = 16
+  let rows  = 30
+  let mines = 99
+
+  game <- genGame cols rows mines (rows `div` 2, cols `div` 2)
+  blankCanvas 3000 (loop game)
+
+loop :: Game -> DeviceContext -> IO ()
+loop game context = send context $ runDraw context $ drawGame game
+
+genGame :: Int -> Int -> Int -> (Int, Int) -> IO Game
+genGame rows columns numMines start = do
+  let bounds    = ((0, 0), (rows-1, columns-1))
+  let positions = [p | p <- range bounds, p /= start]
+  mines <- take numMines <$> shuffleM positions
+
+  return $ Game { rows    = rows
+                , columns = columns
+                , field   = mkMineField bounds mines
+                }
+
+mkMineField :: ((Int, Int), (Int, Int))  -> [(Int, Int)] -> MineField
+mkMineField bounds mines = listArray bounds $ [item p | p <- range bounds]
+  where mineMap = accumArray (||) False bounds [(p, True) | p <- mines]
+
+        neighbors (i, j) = [p | di <- [-1, 0, 1],
+                                dj <- [-1, 0, 1],
+                                let p = (i + di, j + dj),
+                                p /= (i, j),
+                                inRange bounds p]
+
+        hasMine = (mineMap !)
+        item p | hasMine p = Mine
+               | otherwise = Empty (length $ filter hasMine (neighbors p))

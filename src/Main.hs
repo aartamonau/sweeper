7b46431fda5b96ce99f3e32ef4f58225a873ef6d
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2015-12-11 21:51:53 -0800

    stop using cfgSeed

--- src/Main.hs
+++ src/Main.hs
@@ -3,22 +3,14 @@ module Main
          main
        ) where
 
-import System.Random (mkStdGen, setStdGen)
-
-import CmdArgs (Mode(ModeUI, ModeBench), cfgMode, cfgSeed, runWithCfg)
+import CmdArgs (Mode(ModeUI, ModeBench), cfgMode, runWithCfg)
 import qualified Mode.UI as UI
 import qualified Mode.Bench as Bench
 
 main :: IO ()
-main = runWithCfg $ \cfg ->
-  do setSeed cfg
-     dispatch cfg
+main = runWithCfg dispatch
   where dispatch cfg
           | ModeUI    uiCfg    <- mode = UI.run cfg uiCfg
           | ModeBench benchCfg <- mode = Bench.run cfg benchCfg
           | otherwise                  = error "can't happen"
           where mode = cfgMode cfg
-
-        setSeed cfg
-          | Just seed <- cfgSeed cfg = setStdGen (mkStdGen seed)
-          | otherwise                = return ()

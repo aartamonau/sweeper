e3c8b5370e0a7ff25bc2338ec6f7afba098026ff
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2015-12-13 22:31:48 -0800

    reimplement randomSubset without using unsafeInterleaveIO/ST

--- src/Rand.hs
+++ src/Rand.hs
@@ -6,15 +6,21 @@ module Rand
        , systemGen
        , runRand
 
+       , randomSubset
+
        , uniform
        , uniformR
        ) where
 
-import Control.Monad.ST (RealWorld, stToIO)
+import Control.Monad (when)
+import Control.Monad.ST (ST, RealWorld, stToIO)
+import qualified Data.Array.MArray as MArr
+import Data.Array.ST (STArray)
 import Data.Vector (singleton)
 
 import System.Random.MWC (GenST, initialize, withSystemRandom, asGenST)
-import System.Random.MWC.Monad (RandST, uniform, uniformR)
+import qualified System.Random.MWC as R
+import System.Random.MWC.Monad (RandST, uniform, uniformR, toRand)
 import qualified System.Random.MWC.Monad as M
 
 type Rand a = RandST RealWorld a
@@ -29,3 +35,21 @@ systemGen = withSystemRandom $ asGenST (return . Gen)
 
 runRand :: Rand a -> Gen -> IO a
 runRand r = stToIO . M.runRand r . unGen
+
+{-# INLINE randomSubset #-}
+randomSubset :: Int -> [a] -> Rand [a]
+randomSubset k xs
+  | length init < k = return xs
+  | otherwise       = toRand go
+  where (init, rest) = splitAt k xs
+
+        go gen =
+          do arr <- MArr.newListArray (0, k-1) init
+             mapM_ (maybeSwap gen arr) $ zip [k..] rest
+             MArr.getElems arr
+
+        maybeSwap :: GenST RealWorld
+                  -> STArray RealWorld Int a -> (Int, a) -> ST RealWorld ()
+        maybeSwap gen arr (i, x) =
+          do j <- R.uniformR (0, i) gen
+             when (j < k) $ MArr.writeArray arr j x

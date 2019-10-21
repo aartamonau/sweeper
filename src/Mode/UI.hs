aac545b818631102fb803c26576afdfe6802c269
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2019-10-21 01:14:55 -0700

    Globally enabled NamedFieldPuns and DuplicateRecordFields.

--- src/Mode/UI.hs
+++ src/Mode/UI.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE NamedFieldPuns #-}
-
 module Mode.UI
        (
          run
--- src/Player/Constraints.hs
+++ src/Player/Constraints.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 

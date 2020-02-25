765c0915d2b89c75cb2c689657f22724cd791cad
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2020-02-24 22:06:36 -0800

    Add a missing Stats file.

---
+++ src/Stats.hs
@@ -0,0 +1,37 @@
+module Stats
+  ( Stats
+  , numWon
+  , numLost
+  , numStalled
+  , numPlayed
+  , incWon
+  , incLost
+  , incStalled
+  ) where
+
+data Stats =
+  Stats
+    { numWon :: !Int
+    , numLost :: !Int
+    , numStalled :: !Int
+    }
+  deriving (Show)
+
+instance Semigroup Stats where
+  Stats xw xl xs <> Stats yw yl ys = Stats (xw + yw) (xl + yl) (xs + ys)
+
+instance Monoid Stats where
+  mempty = Stats 0 0 0
+
+numPlayed :: Stats -> Int
+numPlayed (Stats {numWon, numLost, numStalled}) =
+  numWon + numLost + numStalled
+
+incWon :: Stats -> Stats
+incWon stats@(Stats {numWon}) = stats {numWon = numWon + 1}
+
+incLost :: Stats -> Stats
+incLost stats@(Stats {numLost}) = stats {numLost = numLost + 1}
+
+incStalled :: Stats -> Stats
+incStalled stats@(Stats {numStalled}) = stats {numStalled = numStalled + 1}

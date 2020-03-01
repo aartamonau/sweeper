c662278ff7601c44a3fa2e145b43dbc0db150173
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2020-02-29 20:53:04 -0800

    Unroll color names in UI.Color.

--- src/UI/Color.hs
+++ src/UI/Color.hs
@@ -26,23 +26,38 @@ type Color = Text
 showColor :: Colour Double -> Text
 showColor = pack . sRGB24show
 
-dimgrey, grey, black, red  :: Color
-blue, green, khaki, purple :: Color
-darkred, brown, lightgrey, darkgrey  :: Color
-[dimgrey, grey, black, red, blue, green,
- khaki, purple, darkred, brown, lightgrey, darkgrey] =
-  map
-    showColor
-    [ Names.dimgrey
-    , Names.grey
-    , Names.black
-    , Names.red
-    , Names.blue
-    , Names.green
-    , Names.khaki
-    , Names.purple
-    , Names.darkred
-    , Names.brown
-    , Names.lightgrey
-    , Names.darkgrey
-    ]
+dimgrey :: Color
+dimgrey = showColor Names.dimgrey
+
+grey :: Color
+grey = showColor Names.grey
+
+black :: Color
+black = showColor Names.black
+
+red :: Color
+red = showColor Names.red
+
+blue :: Color
+blue = showColor Names.blue
+
+green :: Color
+green = showColor Names.green
+
+khaki :: Color
+khaki = showColor Names.khaki
+
+purple :: Color
+purple = showColor Names.purple
+
+darkred :: Color
+darkred = showColor Names.darkred
+
+brown :: Color
+brown = showColor Names.brown
+
+lightgrey :: Color
+lightgrey = showColor Names.lightgrey
+
+darkgrey :: Color
+darkgrey = showColor Names.darkgrey
